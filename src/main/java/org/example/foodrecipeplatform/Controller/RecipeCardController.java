6adbfe879c85072319fb23c53b5693a743643ba8
Vincent Liu <Vincentliu8823@gmail.com>
2025-04-24 22:20:18 -0400

    cards align now when displaying, cards now show in recipe search screen, cards show full name when hovered over it

--- src/main/java/org/example/foodrecipeplatform/Controller/RecipeCardController.java
+++ src/main/java/org/example/foodrecipeplatform/Controller/RecipeCardController.java
@@ -2,6 +2,7 @@ package org.example.foodrecipeplatform.Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import org.example.foodrecipeplatform.CardData;
@@ -19,12 +20,28 @@ public class RecipeCardController {
 
     private CardData cardData;
 
+    @FXML
+    public Tooltip nameTooltip;
+
+    @FXML
+    void initialize()
+    {
+        recipeName.setPrefWidth(200);
+        recipeName.setWrapText(true);
+        recipeName.setPrefHeight(40);
+    }
+
+
     public void setData(CardData cardData) {
         this.cardData = cardData;
         recipeName.setText(cardData.getFoodName());
+
+        nameTooltip.textProperty().setValue(cardData.getFoodName());
+
         //Image image = new Image(getClass().getResourceAsStream( cardData.getImageURL()));
         Image image = new Image(cardData.getImageURL());
         img.setImage(image);
+
         recipeDesc.setText(cardData.getDescription());
     }
 
