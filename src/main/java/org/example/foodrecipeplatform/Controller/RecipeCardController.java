c1577983d0ce637f7aae9e2d1e0b9e93e7cc02f7
Vincent Liu <Vincentliu8823@gmail.com>
2025-04-26 17:39:14 -0400

    cards on click

--- src/main/java/org/example/foodrecipeplatform/Controller/RecipeCardController.java
+++ src/main/java/org/example/foodrecipeplatform/Controller/RecipeCardController.java
@@ -5,10 +5,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import org.example.foodrecipeplatform.CardData;
 
 public class RecipeCardController {
-
+    @FXML
+    public AnchorPane rootPane;
     @FXML
     private ImageView img;
 
@@ -36,13 +38,17 @@ public class RecipeCardController {
         this.cardData = cardData;
         recipeName.setText(cardData.getFoodName());
 
-        nameTooltip.textProperty().setValue(cardData.getFoodName());
+        nameTooltip.textProperty().setValue(cardData.getFoodName()); // showing popup
 
         //Image image = new Image(getClass().getResourceAsStream( cardData.getImageURL()));
         Image image = new Image(cardData.getImageURL());
         img.setImage(image);
 
         recipeDesc.setText(cardData.getDescription());
+
+        rootPane.setOnMouseClicked(event -> { // naking image onclick
+            System.out.println("Card clicked: " + cardData.getFoodName());
+        });
     }
 
 
