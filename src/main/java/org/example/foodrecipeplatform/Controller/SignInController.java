0888970ae3171a611e0c5215da8b80f98a49b3a9
Asim Razzaq <147363287+AsimRazzaq01@users.noreply.github.com>
2025-05-02 17:03:36 -0400

    Profile_Page_Screen (#54)
    * - 123
    
    * sign in fix
    
    * sign in fix
    
    * updated chatgpt integration to fix conflicts while merging
    
    * main commit
    
    * main commit
    
    * created base for Profile Screen
    
    * created base for Profile Screen
    
    * updated
    
    * updated
    
    * create basic features
    - still needs more work on features
    
    * Still needs more work on features
    - need to add bio/db integration
    - need to add profile pic integration
    - need to add favorite cards on screen
    
    * updated comments & methods
    for screens(sign in, register, profile)
    - added methods
    - added pop-ups
    - fixed register user bug
    - fixed sign in issue for new users
    - added more comments
    
    * updated comments & methods
    for screens(sign in, register, profile)
    - added methods
    - added pop-ups
    - fixed register user bug
    - fixed sign in issue for new users
    - added more comments

--- src/main/java/org/example/foodrecipeplatform/Controller/SignInController.java
+++ src/main/java/org/example/foodrecipeplatform/Controller/SignInController.java
@@ -5,6 +5,7 @@ import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -20,51 +21,40 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-public class SignInController
-{
+/**
+ * SignInController class -> logic to get user info from database -> authenticate -> sign in
+ */
+public class SignInController {
     // to connenct buttons, make sure that the id is fx:id= & make sure the controller is set to this class
     @FXML
     private Button sign_inButton;
-
     @FXML
     private Button registerButton;
-
     @FXML
     private TextField usernameTextField;
-
     @FXML
     private PasswordField passwordTextField;
-
     @FXML
     private Text messageText;
-
     @FXML
     private ImageView SignInLogo;
-
-    @FXML
-    private AnchorPane sceneAnchor;
-
     @FXML
     private VBox MainVbox;
 
+    @FXML
+    void showAlert( String header, String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText(header);
+        alert.setTitle(title);
+        alert.setContentText(content);
+        alert.showAndWait();
+    } // End showAlert method
 
 
+    public void initialize() {
 
-    public void initialize()
-    {
+    } // End initialize method
 
-        //MainVbox.setStyle("-fx-background-color: #ff0");
-
-        //fitWidthProperty().bind(MainVbox.widthProperty());
-
-
-
-
-
-        //AccessDataView accessDataViewModel = new AccessDataView();
-        //usernameTextField.textProperty().bindBidirectional(accessDataViewModel.personNameProperty());
-        //register.disableProperty().bind(accessDataViewModel.isWritePossibleProperty().not());
-    }
 
     // to make sure the buttons are connected to the method, add onAction to the button in fxml
     @FXML
@@ -84,60 +74,55 @@ public class SignInController
                 throw new RuntimeException(e);
             }
         }
-        else
+        else {
             System.out.println(username + " is not in database or password wrong");
-    }
+        }
+    } // End sign_inButtonClicked method
 
 
-    @FXML
-    void registerButtonClicked (ActionEvent event) throws IOException {
-        switchToRegisterScreen();
-    }
-
-
-
-
-
-    /*
-        signIn method
-        - takes in username and password which is sent from the username and password text fields.
-        It goes through the firestore database user's document and checks if there's a matching username &
-        password. Returns true if a matching was found & false if not
-        @param username - the email that was taken from the username textfield
-        @param password - the password that was taken from the password textfield
+    /**
+     * signIn method
+     * - takes in username and password which is sent from the username and password text fields.
+     * It goes through the firestore database user's document and checks if there's a matching username &
+     * password. Returns true if a matching was found & false if not
+     * @param username - the email that was taken from the username textfield
+     * @param password - the password that was taken from the password textfield
+     * @return true if input matches database
      */
-    public boolean signIn(String username, String password)
-    {
+    public boolean signIn(String username, String password) {
+
         ApiFuture<QuerySnapshot> future = FoodRecipePlatform.fstore.collection("Users").get();
 
         List<QueryDocumentSnapshot> documents;
-        try
-        {
+        try {
             documents = future.get().getDocuments();
-            if(documents.size()>0)
+
+            if(!documents.isEmpty()) // as longs as documents list ! empty
             {
                 System.out.println("Checking if user is in database...");
 
-                // user and pass are used to store the usernames and passwords in the firestore database
-                String user = "", pass;
-                boolean found = false;
-
-                // going through the document list
                 for (QueryDocumentSnapshot document : documents)
                 {
-                    System.out.println(document.getId() + " => " + document.getData().get("UserName")
-                            + " " + document.getData().get("Password"));
+                    String user = document.getString("UserName");
+                    String pass = document.getString("Password");
 
-                    user = document.getData().get("UserName").toString();
-                    pass = document.getData().get("Password").toString();
+                    System.out.println(document.getId() + " => " + user + " " + pass);
 
-                    // exits the method and returns true when matching username & password was found
                     if (username.equals(user) && password.equals(pass)) {
                         SessionManager.setUserId(document.getId());
                         System.out.println("Logged in as: " + user + " (Doc ID: " + document.getId() + ")");
                         return true;
                     }
+
+                    // optional: you can store if username was found but password wrong
+                    if (username.equals(user) && !password.equals(pass)) {
+                        showAlert("Incorrect Sign In", "Sign in clicked!", username + " has entered an Incorrect Password");
+                        return false;
+                    }
                 }
+
+                // if no match found after checking all
+                showAlert("Incorrect Sign In", "Sign in clicked!", "Sorry, incorrect Username or Password");
             }
             else
             {
@@ -149,16 +134,26 @@ public class SignInController
             ex.printStackTrace();
         }
         return false;
-    }
+    } // End signIn Method
+
+
+    @FXML
+    void registerButtonClicked (ActionEvent event) throws IOException {
+        switchToRegisterScreen();
+    } // End registerButtonClicked button
 
     private void switchToPrimary() throws IOException {
         FoodRecipePlatform.setRoot("HomeScreen");
-    }
-
+    } // End switchToPrimary Method
 
     private void switchToRegisterScreen() throws IOException {
         FoodRecipePlatform.setRoot("RegistrationScreen");
-    }
+    } // End switchToRegisterScreen Method
+
+    @FXML
+    void Forgot_Password_link(ActionEvent event) {
+        showAlert("Follow steps to retrieve Password ", " Forgot Password !", "Sorry at this time we can not retrieve your password !");
+    } // End Forgot_Password_link Method
 
 
-}
+} // End SignInController class
